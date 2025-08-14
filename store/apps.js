366a7db5db13ad959fd64d393b83b2dafa692d6f
John Lin <101694886+tsetse6789@users.noreply.github.com>
2025-08-14 20:22:33 +0800

    Update apps.js

--- store/apps.js
+++ store/apps.js
@@ -100,5 +100,104 @@ Hope you enjoy the app!
                 version: "--"
             }
         ]
+    },
+    {
+        id: "com.lifechurch.bible",
+        name: "YouVersion Bible App + Audio",
+        developer: "Life.Church",
+        category: "Religion",
+        platforms: [
+            {
+                os: "Android",
+                version: "5.0+",
+                deviceType: "Phone, Tablet, PC"
+            },
+            {
+                os: "iOS",
+                version: "16.0+",
+                deviceType: "iPhone, iPad, Apple TV, Apple Vision"
+            }
+        ],
+        icon: "https://play-lh.googleusercontent.com/WS9mwjgQMTxX6WKLP7W1QcjQAjZKLp_h5ENE8MhucSiiN453_po6NW90wLL0c7lNaYw=w480-h960",
+        gallery: [
+            "https://play-lh.googleusercontent.com/AVcz-xNtlKjQruNPDNYytmAWbOh_SLWayrjjTxX_QJTR8pCAZqwAz67DrBAhMtDlpAw=w1052-h592",
+            "https://play-lh.googleusercontent.com/pl-1CUe_GuyFqXbFFHS9ApwHlWCotZ2D4UTRpljrQKSFdbaSh7p_gClMAC6rodX4Gw=w1052-h592",
+            "https://play-lh.googleusercontent.com/YMxbw30u7tM0eRlp2-fNo0wpv158PuVmE4zeZObfPkX8eaiG5uyt20g88rvS7pKtGQ=w1052-h592",
+            "https://play-lh.googleusercontent.com/EesnOWEndd8MTd67tgoMQEYEcN2JVQcEKkR0wlxo-S4j1X3fS6xTyiUlxLKqSzFgbHU=w1052-h592",
+            "https://play-lh.googleusercontent.com/l03bF49gugl061WMC-IVimUlgYMFd4vJh6ifLcD29zFKASqrtPl_B1YtuXM3Qtadyw=w1052-h592",
+            "https://play-lh.googleusercontent.com/m1KrkIVZWPPjVvjtoRWgq9SMjEfnBSL5ffz6w-lj6FMrAm2jFOgUusR8cRMb_k2tWdQ=w1052-h592",
+            "https://play-lh.googleusercontent.com/Ke4zA1Z6lO2zSoKLv9zMfiYa5q0_dcHKBls-4Hi-ljIsDEaoYzU40563jr0y-Fv_POw=w1052-h592",
+            "https://play-lh.googleusercontent.com/X921cLCodfpqHIWDSXUy254AFZajMeMt5_mBCHlTljW7psvKtYGO5r_sn11yCY0KXHBo=w1052-h592"
+        ],
+        intro: "Bible Study + Audio, Verse of Day, Offline, Holy Bible & Daily Devotionals",
+        details: `
+On more than 350 million devices around the world, people are reading, listening to, watching, and sharing the Bible using the #1 rated Bible App—completely free. 1,400+ Bible versions, hundreds of languages. Listen to audio Bibles. Hundreds of Reading Plans & Devotionals, in 40+ languages. Download the best rated Bible App.
+
+Customize your Bible with Highlights, Bookmarks, and public/private Notes. Access everything online or download select versions to use offline. Read, study, and share with friends. Enjoy honest conversations about faith in trusted community. Grow together every day with friends, sharing your discoveries. Create shareable art: add Scripture verses to your photos.
+
+READ THE BIBLE DAILY
+● Experience the Bible App in any of 40+ languages
+● Switch between hundreds of Bible versions, 1000+ languages
+● Popular versions: King James Version KJV, New International Version NIV, New King James Version NKJV, New Living Translation NLT, Revised Standard Version RSV, NRSV, NIV, English Standard Version ESV, NASB, The Message, Reina Valera, Yoruba
+● Offline Bibles: Download and read offline even w/o network access (select versions)
+● Audio Bibles: Audio for select versions. (Not offline)
+● Verse of the Day widget
+
+MAKE PRAYER A DAILY HABIT
+● When you want to pray but you don’t know what to say, Guided Prayer can help you get started.
+● Use Prayer cards to keep track of your Prayer time in the Bible App
+● Easily organize Prayer time lists
+● Make your Prayers private or share with Friends
+● Celebrate answered Prayer requests with your Bible App Friends
+
+USE THE BIBLE WITH FRIENDS
+● Center your Christian friendships around holy Scripture in the Bible App
+● Home Feed: See what you and friends bookmark & highlight
+● Comments: Connect with friends each day as you study Bible truths together, sharing ideas, asking questions
+
+STUDY THE BIBLE
+● Reading Plans: Hundreds of Devotionals, Bible Plans. Study topics or portions of the Bible. Read the entire Bible - One Year® Bible & Bible in One Year.
+● Video: Watch clips from The Bible, the JESUS film, Bible Project, Lumo Project, Spoken Gospel, Streetlights, & more
+
+CUSTOMIZE YOUR FREE BIBLE
+● Verse Images: Turn Scripture into shareable art
+● Highlights: Like a paper Bible with custom colors
+● Bookmarks: Sharing, memorizing, finding your favorite verses
+● Share verses with friends: social media, email, or SMS/text
+● Notes: Keep them private so only you can see them, or public to share with friends
+● Cloud Sync: With a free YouVersion account, see all of your Notes, Highlights, Bookmarks, and Reading Plans on any supported device
+● Easy Reading: Adjust font, text size, and contrast for bright or low-light
+
+CONNECT WITH YOUVERSION
+● Contact support from inside the Bible App
+● Like us on Facebook
+http://facebook.com/youversion
+● Follow us on Twitter
+http://twitter.com/youversion
+● Keep current on our blog
+http://blog.youversion.com
+● YouVersion online
+http://bible.com
+
+More than 1000 languages
+English, Hindi, Spanish, Portuguese, Chinese, Korean, German, French, Russian, Tagalog, Indonesian, Swahili, Arabic, Zulu, Xhosa, Telugu, Yoruba, Urdu, Kikuyu, Filipino, Cebuano, Ilocano, Hiligaynon, Hausa, Twi
+
+Thanks to generous content partners, we offer thousands of Reading Plans, Devotions, and free Bible study plans: Billy Graham, Francis Chan, One Hope, Audio devotions, Our Daily Bread, One Year® Bible, Jesus Film, Lead Like Jesus, Daily Guideposts, Alpha International, Nicky Gumbel HTB Church, Deeper into Scripture, Ligonier, Go Offline, Everyday Essential Verse of the day, Scripture Union, John Piper, Hillsong, Audio books & devotions, Daily Bible Reading Catholic, Daily Hope, Set Me Free, Beth Moore, 21 Days of Our Daily Bread, Audible Devotion, Everyday inspiration, NIV Audiobook, New Christian, Everyday Scripture: Life Application, Hope Gateway, Once a Day Bible Habit, Jesus Calling, Through the Word, BiblePlans.org
+---
+###What's new
+Bug fixes and performance improvements
+        `,
+        downloads: [
+            {
+                type: "Google Play(Android)",
+                url: "https://play.google.com/store/apps/details?id=com.sirma.mobile.bible.android&pcampaignid=web_share",
+                version: "11.6.0+7082775"
+            },
+            {
+                type: "App Store(iOS)",
+                url: "https://apps.apple.com/app/bible/id282935706",
+                version: "11.6.0"
+            }
+        ]
     }
 ];
